8528a843e1c3204da04688cc5ac4bf1a8fd7f0dd
annacritelli <annacritelli19@gmail.com>
2022-10-28 15:26:18 +0200

    java class creation

---
+++ src/test/java/ProvaLaboratorio/it/unical/AnnaCritelli213536/FunnyAlgorithmsTest.java
@@ -0,0 +1,23 @@
+package ProvaLaboratorio.it.unical.AnnaCritelli213536;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FunnyAlgorithmsTest {
+
+	@BeforeClass
+	public void beforeClass() {
+		
+	}
+	
+	@Before
+	public void before() {
+		
+	}
+	
+	@Test
+	public void test1() {
+		
+	}
+}
